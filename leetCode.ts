2ccfbeeb350f7a10bfcd8ac35af16093bf3c329b
sizhong1 <ippo1281@outlook.com>
2023-08-04 09:08:25 +0800

    leetCode

--- leetCode.ts
+++ leetCode.ts
@@ -118,4 +118,113 @@ function sumOfPowers(nums: number[]): number {
 }
 
 // console.log(getArr([658, 489, 777, 2418, 1893, 130, 2448, 178, 1128, 2149, 1059, 1495, 1166, 608, 2006, 713, 1906, 2108, 680, 1348, 860, 1620, 146, 2447, 1895, 1083, 1465, 2351, 1359, 1187, 906, 533, 1943, 1814, 1808, 2065, 1744, 254, 1988, 1889, 1206]));
-console.log(sumOfPower([2, 1, 4]));
+// console.log(sumOfPower([2, 1, 4]));
+// 卡牌翻面
+function flipgame(fronts: number[], backs: number[]): number {
+    const same = new Set();
+    for (let i = 0; i < fronts.length; i++) {
+        if (fronts[i] === backs[i]) {
+            same.add(fronts[i]);
+        }
+    }
+    let res = 3000;
+    for (let x of fronts) {
+        if (x < res && !same.has(x)) {
+            res = x;
+        }
+    }
+    for (let x of backs) {
+        if (x < res && !same.has(x)) {
+            res = x;
+        }
+    }
+    return res % 3000;
+};
+
+// console.log(flipgame([1, 2, 4, 4, 7], [1, 3, 4, 1, 3]));
+
+
+/**
+ * 给你一个整数数组 nums ，请计算数组的 中心下标 。
+    *
+  *  数组 中心下标 是数组的一个下标，其左侧所有元素相加的和等于右侧所有元素相加的和。
+*
+  *  如果中心下标位于数组最左端，那么左侧数之和视为 0 ，因为在下标的左侧不存在元素。这一点对于中心下标位于数组最右端同样适用。
+*
+  *  如果数组有多个中心下标，应该返回 最靠近左边 的那一个。如果数组不存在中心下标，返回 -1 。
+ */
+
+function pivotIndex(nums: number[]): number {
+    let sum = nums.reduce((a, b) => a + b, 0)
+    let left = 0
+    for (let i = 0; i < nums.length; i++) {
+        if (left === sum - left - nums[i]) return i
+        left += nums[i]
+    }
+    return -1
+}
+
+// console.log(pivotIndex([1, 7, 3, 6, 5, 6]));
+
+
+
+/**
+ * 给你两个按 非递减顺序 排列的整数数组 nums1 和 nums2，另有两个整数 m 和 n ，分别表示 nums1 和 nums2 中的元素数目。
+
+请你 合并 nums2 到 nums1 中，使合并后的数组同样按 非递减顺序 排列。
+
+注意：最终，合并后数组不应由函数返回，而是存储在数组 nums1 中。为了应对这种情况，nums1 的初始长度为 m + n，其中前 m 个元素表示应合并的元素，后 n 个元素为 0 ，应忽略。nums2 的长度为 n 。
+ */
+
+function merge(nums1: number[], m: number, nums2: number[], n: number): number[] {
+    if (m === 0) {
+        return nums2
+    }
+    if (n === 0) {
+        return nums1
+    }
+    for (let i = nums1.length - 1; i >= 0; i--) {
+        for (let j = nums2.length - 1; j >= 0; j--) {
+            if (nums1[i] === 0) {
+                nums1[i] = nums2[j]
+                nums2.splice(j, 1)
+                break
+            }
+            if (nums1[i] !== 0) {
+                nums1[i] = nums1[i]
+                break
+            }
+
+        }
+    }
+    return nums1.sort((a, b) => a - b)
+}
+
+// console.log(merge([0, 0, 3, 0, 0, 0, 4, 2, 0], 3, [-1, 1, 1, 1, 2, 3], 6));
+
+
+/**
+ * 
+ * 给你 k 枚相同的鸡蛋，并可以使用一栋从第 1 层到第 n 层共有 n 层楼的建筑。
+
+已知存在楼层 f ，满足 0 <= f <= n ，任何从 高于 f 的楼层落下的鸡蛋都会碎，从 f 楼层或比它低的楼层落下的鸡蛋都不会破。
+
+每次操作，你可以取一枚没有碎的鸡蛋并把它从任一楼层 x 扔下（满足 1 <= x <= n）。如果鸡蛋碎了，你就不能再次使用它。如果某枚鸡蛋扔下后没有摔碎，则可以在之后的操作中 重复使用 这枚鸡蛋。
+
+请你计算并返回要确定 f 确切的值 的 最小操作次数 是多少？
+
+ */
+
+function superEggDrop(k: number, n: number): number {
+    let dp = new Array(k + 1).fill(0).map(() => new Array(n + 1).fill(0))
+    let m = 0
+    while (dp[k][m] < n) {
+        m++
+        for (let i = 1; i <= k; i++) {
+            dp[i][m] = dp[i][m - 1] + dp[i - 1][m - 1] + 1
+        }
+    }
+    return m
+}
+
+console.log(superEggDrop(3, 14));
